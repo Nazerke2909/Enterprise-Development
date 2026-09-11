22a358bc10b4befdd2dc37dc889697a3368e98cb
Nazerke2909 <nazerke.kudaibergen@narxoz.kz>
2026-09-11 16:17:03 +0500

    new branch

--- project/src/main/java/com/example/homework/AssignmentId.java
+++ project/src/main/java/com/example/homework/AssignmentId.java
@@ -1,4 +1,9 @@
 package com.example.homework;
 
 public record AssignmentId(String value) {
+    public AssignmentId {
+        if (value == null || value.isBlank()) {
+            throw new IllegalStateException("Assignment id cannot be null or blank");
+        }
+    }
 }
